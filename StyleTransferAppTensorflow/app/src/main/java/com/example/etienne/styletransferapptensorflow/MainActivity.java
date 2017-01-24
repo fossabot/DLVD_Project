41046d559e8ac440dd5e2d6afa07e186c1701f99
Etienne Buschong <ebuschon@students.uni-mainz.de>
2017-01-24 23:28:03 +0100

    Style Transfer App, Graph Working

--- StyleTransferAppTensorflow/app/src/main/java/com/example/etienne/styletransferapptensorflow/MainActivity.java
+++ StyleTransferAppTensorflow/app/src/main/java/com/example/etienne/styletransferapptensorflow/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,9 +21,11 @@ public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
 
 
-    private static final String MODEL_FILE = "file:///android_asset/tensorflow_inception_graph.pb";
-    private static final String INPUT_NODE = "input:0";
-    private static final String OUTPUT_NODE = "output:0";
+    private static final String MODEL_FILE = "file:///android_asset/gen_export.pb";
+    private static final String INPUT_NODE = "ph_input_image";
+    private static final String OUTPUT_NODE = "output";
+    private static final int DESIRED_HEIGHT = 224;
+    private static final int DESIRED_WIDTH = 224;
 
     TensorFlowInferenceInterface inferenceInterface;
 
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         inferenceInterface = new TensorFlowInferenceInterface();
         inferenceInterface.initializeTensorFlow(getAssets(),MODEL_FILE);
+
 
         imageView = (ImageView) this.findViewById(R.id.imageView);
 
@@ -53,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             if(imageView.getVisibility() == ImageView.INVISIBLE)
                 imageView.setVisibility(ImageView.VISIBLE);
             imageView.setImageBitmap(photo);
-            photo = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher_256);
+            photo = BitmapFactory.decodeResource(getResources(),R.drawable.cat);
             int[] intValues = new int[photo.getHeight()*photo.getWidth()];
             photo.getPixels(intValues, 0, photo.getWidth(), 0, 0, photo.getWidth(), photo.getHeight());
             float[] floatValues = new float[intValues.length * 3];
@@ -64,32 +68,33 @@ public class MainActivity extends AppCompatActivity {
                 floatValues[i * 3 + 2] = (val & 0xFF) / 255.0f;
             }
 
-            Log.d("floatvaluesisze",String.valueOf(floatValues.length));
 
             Log.d("Checkpoint","Get to network");
 
             inferenceInterface.fillNodeFloat(
-                    INPUT_NODE, new int[] {1, photo.getWidth(), photo.getHeight(), 3}, floatValues);
-
+                    INPUT_NODE, new int[] {1, 304, 304, 3}, floatValues);
             Log.d("Checkpoint","Created Input Node");
 
 
             inferenceInterface.runInference(new String[] {OUTPUT_NODE});
-
             Log.d("Checkpoint","Ran inference");
-            inferenceInterface.readNodeFloat(OUTPUT_NODE, floatValues);
 
+            float[] outputValues = new float[DESIRED_WIDTH * DESIRED_HEIGHT * 3];
+
+            inferenceInterface.readNodeFloat(OUTPUT_NODE, outputValues);
             Log.d("Checkpoint","Read Output of Network");
 
-            for (int i = 0; i < intValues.length; ++i) {
-                intValues[i] =
+
+            Bitmap toDraw = Bitmap.createBitmap(DESIRED_WIDTH,DESIRED_HEIGHT, Bitmap.Config.ARGB_8888);
+            int[] colors = new int[DESIRED_WIDTH*DESIRED_HEIGHT];
+            for (int i = 0; i < colors.length; i ++ ) {
+                colors[i] =
                         0xFF000000
-                                | (((int) (floatValues[i * 3] * 255)) << 16)
-                                | (((int) (floatValues[i * 3 + 1] * 255)) << 8)
-                                | ((int) (floatValues[i * 3 + 2] * 255));
+                                | (((int) (outputValues[i * 3] * 255)) << 16)
+                                | (((int) (outputValues[i * 3 + 1] * 255)) << 8)
+                                | ((int) (outputValues[i * 3 + 2] * 255));
             }
-            Bitmap toDraw = photo.copy(photo.getConfig(),true);
-            toDraw.setPixels(intValues, 0, photo.getWidth(), 0, 0, photo.getWidth(), photo.getHeight());
+            toDraw.setPixels(colors, 0, toDraw.getWidth(), 0, 0, toDraw.getWidth(), toDraw.getHeight());
             imageView.setImageBitmap(toDraw);
         }
     }
